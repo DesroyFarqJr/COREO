720a49a97459c7f95a235634f66d8476135e96b6
Manzamusajr <110345269+Manzamusajr@users.noreply.github.com>
2023-03-20 21:53:53 -0400

    Front-end 1st draft

--- project/vue/src/App.vue
+++ project/vue/src/App.vue
@@ -1,9 +1,76 @@
 <template>
+
   <div id="app">
     <div id="nav">
+      <nav class="navbar navbar-expand-md navbar-light fixed-top">
+      <div class="container-fluid">
+        <!-- <a href="#" class="navbar-brand"
+          ><img alt="logo" src="../../public/images/logo.png"
+        /></a> -->
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="navbarsExampleDefault"
+          label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="_collapse _navbar-collapse">
+          <!-- ^^ class name changed...rename if needed -->
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+              <button class="nav-link" style="" >Upload</button>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" to="/logout"> Log out</router-link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
       <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
       <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
     </div>
     <router-view />
   </div>
 </template>
+
+<style scoped>
+.navbar-brand img {
+  display: flex;
+  justify-content: flex-start;
+  height: 50px;
+  width: 200px;
+  padding: 0;
+  margin: 0;
+  left: 0;
+}
+.navbar-brand {
+  text-transform: uppercase;
+  color:rgb(255, 137, 3);
+}
+.navbar-light {
+  display: flex;
+  flex-direction: column;
+  flex-wrap: nowrap;
+  justify-content: flex-end;
+  align-items: baseline;
+  align-content: stretch;
+  border-bottom: 2px solid transparent;
+
+  background-color: #ffffff;
+  box-shadow: 0px 5px 5px #7f7d7e;
+}
+.navbar-collapse {
+  width: 100%;
+}
+
+a {
+  box-shadow: inset 0 0 0 0 #000000;
+  color: #999999;
+  padding: 0 0.25rem;
+  margin: 0 -0.25rem;
+  transition: color 0.3s ease-in-out, box-shadow 0.3s ease-in-out;
+}
+</style>
